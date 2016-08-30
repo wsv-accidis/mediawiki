c256c2f3cd0737d1ff18ea0a160bfe8beca7fbdf
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2016-08-30 23:08:34 +0200

    Updated HTMLets and added FB SDK to skin.

--- skins/tmeit/tmeit.php
+++ skins/tmeit/tmeit.php
@@ -110,6 +110,14 @@ class TmeitTemplate extends BaseTemplate
 ?>
 		<div class="cleared"></div>
 		<div id="main">
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.7";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>
 <?
 		$this->renderContent();
 		if( $this->areWeOnMainPage || $this->doesPageHaveCategories ):
