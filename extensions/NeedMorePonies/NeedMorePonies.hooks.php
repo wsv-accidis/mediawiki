de980c07dd635cf1fa7f6f48e0fbecd28466ac09
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2016-09-19 19:56:02 +0200

    Ensure NeedMorePonies loads over HTTPS to avoid error messages about broken security.

--- extensions/NeedMorePonies/NeedMorePonies.hooks.php
+++ extensions/NeedMorePonies/NeedMorePonies.hooks.php
@@ -9,8 +9,8 @@ class NeedMorePoniesHooks
 {
 	public static function beforePageDisplay( OutputPage &$out, Skin &$skin )
 	{
-		$out->addScriptFile( 'http://panzi.github.com/Browser-Ponies/basecfg.js' );
-		$out->addScriptFile( 'http://panzi.github.com/Browser-Ponies/browserponies.js' );
+		$out->addScriptFile( 'https://panzi.github.com/Browser-Ponies/basecfg.js' );
+		$out->addScriptFile( 'https://panzi.github.com/Browser-Ponies/browserponies.js' );
 		$out->addModules( 'ext.NeedMorePonies' );
 		return true;
 	}
